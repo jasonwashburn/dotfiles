e4474478d64d61e7936e8f2eaa208f5869d45816
Jason Washburn <jason.washburn@gmail.com>
2022-12-17 15:41:46 -0600

    adds harpoon keys and tmux cht sht

--- dot_config/nvim/lua/wburn/remap.lua
+++ dot_config/nvim/lua/wburn/remap.lua
@@ -1,6 +1,6 @@
 local nnoremap = require("wburn.keymap").nnoremap
 local tnoremap = require("wburn.keymap").tnoremap
-nnoremap("<leader>ls", "<cmd>Ex<CR>")
+nnoremap("<leader>d", "<cmd>Ex<CR>")
 
 nnoremap("<leader>ff", "<cmd>Telescope find_files<cr>")
 nnoremap("<leader>fg", "<cmd>Telescope live_grep<cr>")
@@ -12,10 +12,10 @@ nnoremap("<C-_>", "<cmd>Telescope current_buffer_fuzzy_find sorting_strategy=asc
 nnoremap("<leader>a", function() require("harpoon.mark").add_file() end, silent)
 nnoremap("<leader>e", function() require("harpoon.ui").toggle_quick_menu() end, silent)
 
-nnoremap("<leader>1", function() require("harpoon.ui").nav_file(1) end, silent)
-nnoremap("<leader>2", function() require("harpoon.ui").nav_file(2) end, silent)
-nnoremap("<leader>3", function() require("harpoon.ui").nav_file(3) end, silent)
-nnoremap("<leader>4", function() require("harpoon.ui").nav_file(4) end, silent)
+nnoremap("<leader>j", function() require("harpoon.ui").nav_file(1) end, silent)
+nnoremap("<leader>k", function() require("harpoon.ui").nav_file(2) end, silent)
+nnoremap("<leader>l", function() require("harpoon.ui").nav_file(3) end, silent)
+nnoremap("<leader>;", function() require("harpoon.ui").nav_file(4) end, silent)
 nnoremap("<leader>t", function() require("harpoon.term").gotoTerminal(1) end, silent)
 
 tnoremap("<Esc>", "<C-\\><C-n>")
