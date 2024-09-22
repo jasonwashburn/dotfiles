4e63b4e4479aaa37e9005c2ab4285ecf20c3494b
Jason Washburn <jason.washburn@gmail.com>
2024-09-22 09:08:37 -0500

    some updates to zsh, plugins, and wezterm maximize

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -10,9 +10,14 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
+-- set default window to maximize on open
+local mux = wezterm.mux
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
 
--- For example, changing the color scheme:
+-- Changing the color scheme:
 config.color_scheme = "Dracula (Official)"
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
