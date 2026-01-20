726fd0a5ff7c83d07c3a58393f93dc88948a1e61
Jason Washburn <jason@defenseunicorns.com>
2026-01-20 09:07:04 -0600

    chore: add changes to ghostty, godoc, and global gitconfig

--- dot_config/nvim/lua/plugins/godoc.lua
+++ dot_config/nvim/lua/plugins/godoc.lua
@@ -1,20 +1,22 @@
 return {
-	"fredrikaverpil/godoc.nvim",
-	version = "*",
-	dependencies = {
-		{ "folke/snacks.nvim" }, -- optional
-		{
-			"nvim-treesitter/nvim-treesitter",
-			opts = {
-				ensure_installed = { "go" },
-			},
-		},
-	},
-	--    build = "go install github.com/lotusirous/gostdsym/stdsym@latest", -- optional
-	cmd = { "GoDoc" }, -- optional
-	opts = {
-		picker = {
-			type = "snacks",
-		},
-	},
+  "fredrikaverpil/godoc.nvim",
+  version = "*",
+  dependencies = {
+    { "nvim-telescope/telescope.nvim" }, -- optional
+    { "folke/snacks.nvim" }, -- optional
+    { "nvim-mini/mini.pick" }, -- optional
+    {
+      "nvim-treesitter/nvim-treesitter",
+      opts = {
+        ensure_installed = { "go" },
+      },
+    },
+  },
+  --    build = "go install github.com/lotusirous/gostdsym/stdsym@latest", -- optional
+  cmd = { "GoDoc" }, -- optional
+  opts = {
+    picker = {
+      type = "snacks",
+    },
+  },
 }
