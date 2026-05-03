19ff3bd282da01bdef599416c8d89b3d9b5d9f69
Jason Washburn <jason.washburn@gmail.com>
2026-05-03 15:01:59 -0500

    fix: finally fixed ghost text in lazyvim copilot completions

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,23 @@
 return {
   { "Mofiqul/dracula.nvim" },
-
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "dracula",
     },
+  },
+  {
+    -- Hook into colorscheme load to override highlights
+    "Mofiqul/dracula.nvim",
+    config = function()
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        pattern = "dracula",
+        callback = function()
+          vim.api.nvim_set_hl(0, "LspInlayHint", { fg = "#6272a4", italic = true })
+        end,
+      })
+      require("dracula").setup({})
+      vim.cmd("colorscheme dracula")
+    end,
   },
 }
